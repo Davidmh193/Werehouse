6fac9877100c4a9f9484bd840c89da1533e2a115
David Mangas <David Mangas@HZ114824.H012108.NET>
2023-01-09 14:12:27 +0100

    Cambiado almacen y hecho mas

--- src/WereHOUSE/Almacen.java
+++ src/WereHOUSE/Almacen.java
@@ -1,10 +1,33 @@
 package WereHOUSE;
 
+import java.util.ArrayList;
+
 public class Almacen {
-	public void cargarDatos(String[] args){
+	public void cargarDatos(String[] args) {
+
+	}
+
+	public Articulo elMasCaro() {
+		Articulo x = null;
+		
+		return x;
+	}
+
+	public double precio(String codigoProducto) {
+		return 0;
+	}
+	public boolean hayStock(String codigoProducto) {
+		return false;
+	}
+	public ArrayList stockJusto() {
+		ArrayList x = new ArrayList();
+		return x;
+	}
+	
+	public Articulo articulo() {
+		Articulo x =null;
+		
+		return x;
 		
 	}
-	public Articulo elMasCaro[];
-
 }
-	public precio  
